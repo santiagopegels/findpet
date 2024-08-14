93db55369d89ff2674d9665751e7a8b5815ba9e2
Santiago Pegels <santiagopegels@gmail.com>
2024-08-14 10:50:17 -0300

    Add phone field into search model

--- models/search.js
+++ models/search.js
@@ -17,6 +17,10 @@ const searchSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  phone: {
+    type: String,
+    required: true
+  },
   type: {
     type: String,
     required: true,
