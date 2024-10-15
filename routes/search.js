04c1ad0d090a3d8175bc7e8203346bf6024cd6d7
Santiago Pegels <santiagopegels@gmail.com>
2024-10-15 11:50:51 -0300

    add express validation on routes

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,28 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
-const { createSearch, getAllSearches, reverseSearch } = require('../controllers/search')
+const { createSearch, getAllSearches, reverseSearch } = require('../controllers/search');
+const { validateFields } = require('../middleware/validate-fields');
 
 const router = Router();
 
-router.get('/',
-  getAllSearches)
+router.get('/',  getAllSearches)
 
-  router.post('/reverse-search',
-    reverseSearch)
+router.post('/reverse-search',
+  [
+    check('city', 'City is required').notEmpty(),
+    validateFields
+  ],
+  reverseSearch)
 
 router.post('/',
   [
-      check('description', 'Name is required').notEmpty(),
+    check('city', 'City is required').notEmpty(),
+    check('gpsLocation.latitude', 'Latitude is required').notEmpty().isFloat(),
+    check('gpsLocation.longitude', 'Longitude is required').notEmpty().isFloat(),
+    check('phone', 'Phone is required').notEmpty(),
+    check('type', 'Type is required').notEmpty().isIn(['FIND', 'LOST']),
+    check('image', 'Image is required').notEmpty().isBase64(),
+    validateFields
   ],
   createSearch)
 
